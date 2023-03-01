3d8bf69de126241e0b36f99fdd7531aa9d3103bb
Yeong <yjo0723@hotmail.com>
2023-02-28 18:57:49 -0800

    MovieCard styling

--- src/components/MovieCard.jsx/MovieCard.jsx
+++ src/components/MovieCard.jsx/MovieCard.jsx
@@ -1,4 +1,6 @@
 import styles from './MovieCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 const IMAGE_WIDTH = 300;
 
@@ -13,7 +15,13 @@ export default function MovieCard({ movie }) {
       <img className={styles.img} src={posterPath} alt={title}></img>
       <section className={styles.content}>
         <div className={styles.title}>{title}</div>
-        <span>{year}</span> <span>rating: {voteAverage}</span>
+        <div className={styles.footer}>
+          <span className={styles.date}>{year}</span>
+          <div>|</div>
+          <span className={styles.rating}>
+            <FontAwesomeIcon icon={faStar} /> {voteAverage}
+          </span>
+        </div>
       </section>
     </div>
   );
