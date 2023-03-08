093474fa6d80636dd1bccf9434bba7c869dd2050
Yeong <yjo0723@hotmail.com>
2023-03-08 12:36:18 -0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,7 @@ import { formatMedia } from '../utils';
 const tmdb = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    // api_key: import.meta.env.VITE_TMDB_API_KEY,
-    api_key: VITE_TMDB_API_KEY,
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
     language: 'en-US',
   },
 });
